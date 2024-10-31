ab8cb63d91bd44730ab1a1b6837737ca427a77ac
Luc <luc001@e.ntu.edu.sg>
2024-10-31 12:21:11 +0800

    First commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+# Create a build directory if it doesn't exist
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+# Navigate to the build directory
+cd build
+
+# Run CMake to configure the project
+cmake ..
+
+# Build the project
+make
+
+cd ..
